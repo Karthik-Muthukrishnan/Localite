a825f471dec6fc7e491a9bbeaef77a7a24462eda
Karthik Muthukrishnan <reachkarti@gmail.com>
2013-10-13 00:04:35 +0530

    Initial Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plocalite::Application.config.secret_key_base = '8b61d1b5d9e262b068fbcce7045a3c3ad61abd8e23a804aea057b62fe804ddb23490e5aea5d93d97d9e4f89b9012169433fcced6e4cc33bfaa9888502cff4412'
